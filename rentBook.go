a71b8c2f09aa2856ebc0e851f586264a60f2d09e
Himasai Reddy <bhsr2003@gmail.com>
2023-07-02 16:21:55 +0530

    Returning book is done

--- rentBook.go
+++ rentBook.go
@@ -84,5 +84,37 @@ func takeBook(coll *mongo.Collection, ctx context.Context, reqID int32) {
 }
 
 func ReturnBook(coll *mongo.Collection, ctx context.Context) {
+	fmt.Println("Enter the ID of book you want to return:- ")
+	var returnID int32
+	fmt.Scan(&returnID)
+
+	filter := bson.M{"bookId": returnID}
+	var book bson.M
+	err := coll.FindOne(ctx, filter).Decode(&book)
+
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			fmt.Println("Book not found.")
+		} else {
+			panic(err)
+		}
+		return
+	}
+
+	fmt.Println("\nHere are the details of the book you are returning:- ")
+	PrintBookDetails(book)
+
+	quantity, _ := book["quantity"].(int32)
+
+	quantity++
+	fmt.Printf("New quantity: %d\n", quantity)
+
+	update := bson.M{"$set": bson.M{"quantity": quantity}}
+	_, err = coll.UpdateOne(ctx, filter, update)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Thank you for returning %v book!!\n", book["name"])
 
 }
