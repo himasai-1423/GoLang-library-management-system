0db22154b9fca6f95568605243fd55efc773a22d
Himasai Reddy <bhsr2003@gmail.com>
2023-07-03 12:42:07 +0530

    Completed MVC model

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	controller "lib-mng-sys/controllers"
 	model "lib-mng-sys/models"
+	"lib-mng-sys/views"
 )
 
 func main() {
@@ -21,7 +22,7 @@ func main() {
 
 	shouldExit := false
 	for !shouldExit {
-		choice, userName := userChoice()
+		choice, userName := views.UserChoice()
 
 		switch choice {
 		case 1:
@@ -35,25 +36,6 @@ func main() {
 			shouldExit = true
 		}
 	}
-}
-
-func userChoice() (int, string) {
-	fmt.Println("\nWelcome to Library Management System!")
-
-	fmt.Println("\n\nPlease enter your name: ")
-	var userName string
-	fmt.Scan(&userName)
-
-	fmt.Printf("\n Hello! %v,", userName)
-	fmt.Println("\nPlease choose any of the option below:-")
-	fmt.Println("1. Rent a book ")
-	fmt.Println("2. Return a book")
-	fmt.Println("3. Read a book ")
-	fmt.Println("Any other key to exit")
-
-	var choice int
-	fmt.Scan(&choice)
-	return choice, userName
 }
 
 func Leaving(userName string) {
---
+++ views/userChoice.go
@@ -0,0 +1,22 @@
+package views
+
+import "fmt"
+
+func UserChoice() (int, string) {
+	fmt.Println("\nWelcome to Library Management System!")
+
+	fmt.Println("\n\nPlease enter your name: ")
+	var userName string
+	fmt.Scan(&userName)
+
+	fmt.Printf("\n Hello! %v,", userName)
+	fmt.Println("\nPlease choose any of the option below:-")
+	fmt.Println("1. Rent a book ")
+	fmt.Println("2. Return a book")
+	fmt.Println("3. Read a book ")
+	fmt.Println("Any other key to exit")
+
+	var choice int
+	fmt.Scan(&choice)
+	return choice, userName
+}
